f57590fb452d8f2c9318db25547dfa1f0163de69
VMurtazin <obdorsk@gmail.com>
2021-11-25 21:26:24 +0500

    final

--- src/main/java/ru/murtazin/security/configuration/MySecurityConfig.java
+++ src/main/java/ru/murtazin/security/configuration/MySecurityConfig.java
@@ -1,6 +1,7 @@
 package ru.murtazin.security.configuration;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,16 +9,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder= User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-        .withUser(userBuilder.username("slava").password("slava").roles("EMPLOYEE"))
-        .withUser(userBuilder.username("sveta").password("sveta").roles("HR"))
-        .withUser(userBuilder.username("super").password("super").roles("MANAGER","HR"));
+
+        auth.jdbcAuthentication().dataSource(dataSource);
+
+//        UserBuilder userBuilder= User.withDefaultPasswordEncoder();
+//        auth.inMemoryAuthentication()
+//        .withUser(userBuilder.username("slava").password("slava").roles("EMPLOYEE"))
+//        .withUser(userBuilder.username("sveta").password("sveta").roles("HR"))
+//        .withUser(userBuilder.username("super").password("super").roles("MANAGER","HR"));
 
     }
 
